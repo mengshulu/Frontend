cc9aac049740c6725612527503004030efc778c6
RShu <breezecoo1123@gmail.com>
2023-05-15 16:52:33 +0800

    Feat: 刪去沒用到的部分

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="Home">
+  </div>
 </template>
+
+<style>
+</style>
